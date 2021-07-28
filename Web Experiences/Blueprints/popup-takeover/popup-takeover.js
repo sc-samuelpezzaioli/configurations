bee7e893c4cb2c218d937eb2537156c302416ead
Samuel270 <samuel.pezzaioli@sitecore.com>
2021-07-28 15:07:41 +0200

    pup-up

--- Web Experiences/Blueprints/popup-takeover/popup-takeover.js
+++ Web Experiences/Blueprints/popup-takeover/popup-takeover.js
@@ -1,24 +1,44 @@
-insertHTMLBefore("body");
-function sendDataToBoxever(eventType) {
-    var eventToSent = {
+// Adds a unique variant identifier to CSS when deployed to ensure CSS does not impact styling of other elements.
+var compiledCSS = Boxever.templating.compile(variant.assets.css)(variant);
+var styleTag = document.getElementById('style-' + variant.ref);
+if (styleTag) {
+    styleTag.innerHTML = compiledCSS;
+}
+// End Adds a unique variant identifier to CSS when deployed to ensure CSS does not impact styling of other elements.
+
+//declare function to send data to SITECORE CDP
+const sendInteractionToBoxever = function(interactionType) {
+    let eventToSent = {
         "channel": "WEB",
-        "type": eventType,
+        "type": "[[ Experience ID | String | POP_UP | {required: true , group: General}]]_" + interactionType,
         "pos": window._boxever_settings.pointOfSale,
         "browser_id": Boxever.getID(),
-        "interactionID":"OOB_EXP",
-	    "interactionName": "POP_UP_TAKEOVER"
     };
     Boxever.eventCreate(eventToSent, function (data) { }, 'json');
 }
+
+// insert html into body
+insertHTMLBefore("body");
 document.querySelector("#bx-modal_overlay").style.display = "flex";
 document.body.classList.add("bx-modal_content");
+
+
+//declaration and listener in case if dismissing the experience
 var bxCardClose = document.querySelector(".bx-modal__btn-close-icon");
 bxCardClose.onclick = function() {
-    sendDataToBoxever("INTERACTION_DISMISSED");
+    sendInteractionToBoxever("DISMISSED");
     document.querySelector("#bx-modal_overlay").style.display = "none";
 }
+
+//declaration and listener in case POP-UP get engaged
 var bxCardClicked = document.querySelector(".bx-modal_button");
 bxCardClicked.onclick = function() {
-    sendDataToBoxever("INTERACTION_CLICKED");
+    sendInteractionToBoxever("CLICKED");
+    document.querySelector("#bx-modal_overlay").style.display = "none";
+}
+//declaration and listener in case if dismissing the experience clicking outside
+var overlayClicked = document.querySelector("#bx-modal_overlay");
+overlayClicked.onclick = function() {
+    sendInteractionToBoxever("DISMISSED");
     document.querySelector("#bx-modal_overlay").style.display = "none";
 }
