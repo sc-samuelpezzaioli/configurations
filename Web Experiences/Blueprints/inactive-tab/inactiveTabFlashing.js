57808d9da3d1b1667a7d38b47242e4f7650e082b
Samuel270 <samuel.pezzaioli@sitecore.com>
2021-07-28 15:20:18 +0200

    inactive tab

--- Web Experiences/Blueprints/inactive-tab/inactiveTabFlashing.js
+++ Web Experiences/Blueprints/inactive-tab/inactiveTabFlashing.js
@@ -1,18 +1,27 @@
-insertHTMLBefore("body");
+//copy and paste this snipet into your JS file of the experience content variant
+//and the Advance pagetargeting snipet into your page tergeting script
+//configure the text message using the UI
+
+//declarations
 var pageTitle = document.title;
-var attentionMessage = "[[Title Text | text |Choose first flashing text| {order: 1,max: 20}]]";
-var attentionMessage1 = "[[Second Text | text |Choose second flashing text| {order: 1,max: 20}]]"
+// default messages
+var attentionMessage = "[[Title Text | text |Choose first flashing text| {max: 20}]]";
+var attentionMessage1 = "[[Second Text | text |Choose second flashing text| {max: 20}]]"
 var blinkEvent = null;
 var eventSent = false;
-
 var isPageActive = !document.hidden;
-if(!isPageActive){
-    blink();
-}else {
-    document.title = pageTitle;
-    clearInterval(blinkEvent);
+
+
+//functions declarations
+const blink = function(){
+    blinkEvent = setInterval(function() {
+        if(document.title === attentionMessage1){
+            document.title = attentionMessage;
+        }else {
+            document.title = attentionMessage1;
+        }
+    }, 100);
 }
-window.addEventListener('visibilitychange', visibilityChangeFunction);
 function visibilityChangeFunction(){
     var isPageActive = !document.hidden;
     if(!isPageActive){
@@ -23,12 +32,25 @@ function visibilityChangeFunction(){
     }
 }
 
-function blink(){
-    blinkEvent = setInterval(function() {
-        if(document.title === attentionMessage1){
-            document.title = attentionMessage;
-        }else {
-            document.title = attentionMessage1;
-        }
-    }, 100);
+// BX function declaration
+const sendInteractionToBoxever = function(interactionType) {
+    let eventToSent = {
+        "channel": "WEB",
+        "type": "[[ Experience ID | String | INACTIVETAB | {required: true}]]_" + interactionType,
+        "pos": window._boxever_settings.pointOfSale,
+        "browser_id": Boxever.getID(),
+    };
+    Boxever.eventCreate(eventToSent, function (data) { }, 'json');
 }
+//end functions declarations
+
+
+blink();
+sendInteractionToBoxever("TRIGGERED");
+
+//this following listener function will stop the experience if user come back to the page
+// and retrigger if leave again
+window.addEventListener('visibilitychange', visibilityChangeFunction);
+
+
+
